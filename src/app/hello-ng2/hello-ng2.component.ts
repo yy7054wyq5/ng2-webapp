0e12c091f3b6d78761595d541a3aa5e21252af62
吴阳 <349413799@qq.com>
2017-02-08 16:25:53 +0800

    ..

--- src/app/hello-ng2/hello-ng2.component.ts
+++ src/app/hello-ng2/hello-ng2.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./hello-ng2.component.css']
 })
 export class HelloNg2Component implements OnInit {
-  @Input() data;
+  @Input() data;//输入绑定
   constructor() { }//类的构造函数会在所有其它生命周期钩子之前调用。使用它来注入依赖，但是要避免用它做较重的工作。
   ngOnInit() {
     console.log(this.data);
