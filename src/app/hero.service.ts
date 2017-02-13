008b6084d94763f61e2fa1e68a41151113070e53
吴阳 <349413799@qq.com>
2017-02-13 11:52:20 +0800

    服务返回promise

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -5,7 +5,12 @@ import { HEROES } from './mock-heroes';
 
 @Injectable()
 export class HeroService {
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Promise<Hero[]> {
+    return Promise.resolve(HEROES);
+  };
+  getHeroesSlowly(): Promise<Hero[]> {
+  return new Promise<Hero[]>(resolve =>
+    setTimeout(resolve, 1000)) // delay 2 seconds
+    .then(() => this.getHeroes());
   }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,8 +13,13 @@ export class HomeComponent implements OnInit {
   constructor(
     private HeroService: HeroService,
   ){}
+  
+  getHeroes(): void{//函数
+    this.HeroService.getHeroesSlowly().then(heroes => this.heroes = heroes);//接受promise
+  }
+
   ngOnInit() {
-    this.heroes = this.HeroService.getHeroes();
+    this.getHeroes();//调用函数
     //onselect(hero: Hero): void {}
   }
 
