7a066c6a7cce82b5c928188919a2a83b76f29b4e
吴阳 <349413799@qq.com>
2017-02-20 17:48:08 +0800

    http.get

--- src/app/view/hero-detail/hero-detail.component.ts
+++ src/app/view/hero-detail/hero-detail.component.ts
@@ -1,6 +1,11 @@
+import { Hero } from './../../class/hero';
+import { Http, Response } from '@angular/http';
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params ,Data } from '@angular/router';
+
 import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/toPromise';
+
 import { HeroService } from '../../service/hero.service';
 import { Location } from '@angular/common';
 
@@ -14,11 +19,13 @@ export class HeroDetailComponent implements OnInit {
   hero;
   heroes;
   title;
+  data;
   constructor(//类的构造函数会在所有其它生命周期钩子之前调用。使用它来注入依赖，但是要避免用它做较重的工作。
     private route: ActivatedRoute,
     private router: Router,
     private heroService: HeroService,
-    private location: Location
+    private location: Location,
+    private http: Http,
   ) {}
 
   goBack(): void {
@@ -31,7 +38,15 @@ export class HeroDetailComponent implements OnInit {
     .subscribe(hero => this.hero = hero)
     this.route.data
     .subscribe((data: Data) => this.title = data['title'])//接受路由中的数据
-    
+
+    this.http.get('http://scmpurchase.loongjoy.com/api/purchaseOrder/getList?fromSys=scmpcapp&lang=zh&pageIndex=1&pageSize=10&token=feed196d333adee733e6fa889c9188eb')
+      .subscribe(response => {
+        let aa = response.json().data;
+        console.log(aa);
+      },
+        (err)=>console.log(err),
+        ()=>console.log("Done")
+      )
   }
 
 }
