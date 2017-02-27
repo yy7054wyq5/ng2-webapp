92a85e7ec2adcbdafcdf2c88bfa53a0881fd90ba
吴阳 <349413799@qq.com>
2017-02-27 13:08:16 +0800

    api

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -1,11 +1,21 @@
 import { Http, Response } from '@angular/http';
 import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map';
 
 @Injectable()
-export class HttpService {
+export class ApiService {
 
   constructor(
     public http: Http,
   ) { }
+
+  ajax(options) {
+    return this.http.request(options.url, options)
+            .subscribe(
+              (res: Response) => {
+                return res.json();
+              }
+            );
+  };
 
 }
--- src/app/view/home/home.component.ts
+++ src/app/view/home/home.component.ts
@@ -2,12 +2,13 @@ import { Http } from '@angular/http';
 import { Component, OnInit } from '@angular/core';
 import { HeroService } from '../../service/hero.service';
 import { StorageService } from '../../service/storage.service';
+import { ApiService } from '../../service/api.service';
 
 @Component({
   selector: 'app-home-component',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.less'],
-  providers: [HeroService, StorageService]
+  providers: [HeroService, StorageService, ApiService]
 })
 export class HomeComponent implements OnInit {
   appInfo;
@@ -15,7 +16,8 @@ export class HomeComponent implements OnInit {
   constructor(
     private HeroService: HeroService,
     private http: Http,
-    private storage: StorageService
+    private storage: StorageService,
+    private api: ApiService
   ) { };
   testGet(): Promise<any> {
     return this.http.get('/api/app/info/11?sign=beb790d872f5b20202c7d4e98119c54d&timeout=5000')
@@ -26,21 +28,29 @@ export class HomeComponent implements OnInit {
   };
   ngOnInit() {
     this.appInfo = [];
-    this.testGet()
-      .then(res => {
-        if (res.success) {
-          for (const key in res.content) {
-            if (res.content.hasOwnProperty(key)) {
-              this.appInfo.push(res.content[key]);
-            }
-          }
-          this.storage.put({
-            type: 'local',
-            key: 'appinfo',
-            data: res.content
-          });
-        }
-      });
+    this.api.ajax({
+      method: 'get',
+      url: '/api/app/info/11',
+      options: {
+        timeout: 5000,
+        sign: 'beb790d872f5b20202c7d4e98119c54d'
+      }
+    });
+    // this.testGet()
+    //   .then(res => {
+    //     if (res.success) {
+    //       for (const key in res.content) {
+    //         if (res.content.hasOwnProperty(key)) {
+    //           this.appInfo.push(res.content[key]);
+    //         }
+    //       }
+    //       this.storage.put({
+    //         type: 'local',
+    //         key: 'appinfo',
+    //         data: res.content
+    //       });
+    //     }
+    //   });
 
   }
 
