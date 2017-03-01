7a1452a466003309025c9223432e9ddbbc1234ab
吴阳 <349413799@qq.com>
2017-03-01 11:05:59 +0800

    ..

--- src/app/view/detail/detail.component.ts
+++ src/app/view/detail/detail.component.ts
@@ -1,13 +1,9 @@
 import { ApiService } from './../../service/api.service';
-import { Hero } from './../../class/hero';
-import { Http, Response } from '@angular/http';
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute, Params , Data } from '@angular/router';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/switchMap';
-import 'rxjs/add/operator/toPromise';
-
+import { ActivatedRoute, Params , Data } from '@angular/router';
 import { Location } from '@angular/common';
+
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'app-detail-component',
@@ -20,7 +16,6 @@ export class DetailComponent implements OnInit {
   detail;
   constructor( // 类的构造函数会在所有其它生命周期钩子之前调用。使用它来注入依赖，但是要避免用它做较重的工作。
     private route: ActivatedRoute,
-    private router: Router,
     private location: Location,
     private api: ApiService,
   ) {}
