397f0707850869e472b60d70af1cc209ec39fdfa
吴阳 <349413799@qq.com>
2017-03-01 11:08:07 +0800

    ...

--- src/app/component/hello-ng2/hello-ng2.component.ts
+++ src/app/component/hello-ng2/hello-ng2.component.ts
@@ -12,7 +12,6 @@ export class HelloNg2Component implements OnInit {
   constructor() { } // 类的构造函数会在所有其它生命周期钩子之前调用。使用它来注入依赖，但是要避免用它做较重的工作。
   ngOnInit() {
     this.info = this.data;
-    console.log(this.data);
     this.look = function () {
       console.log(this.info);
     };
