a32cc19faddbdbefd818e11c33071b3f7083f4da
吴阳 <yang_wu@loongjoy.com>
2017-04-03 12:37:53 +0800

    搜索

--- src/app/product/index/index.component.ts
+++ src/app/product/index/index.component.ts
@@ -1,3 +1,5 @@
+import { StorageService } from './../../service/storage.service';
+import { ApiService } from './../../service/api.service';
 import { flyIn } from './../../animation/fly-in';
 import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
@@ -11,12 +13,17 @@ import { Component, OnInit } from '@angular/core';
 export class ProductIndexComponent implements OnInit {
   list;
   defaultImage = 'assets/lazy_default.png';
+  keyword;
+  order;
+  orderType;
   lineLeaveLeft;
   timeDirection;
   saleDirection;
   priceDirection;
   constructor(
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private api: ApiService,
+    private storage: StorageService
   ) { }
 
   changeDirection(direction) {
@@ -30,23 +37,62 @@ export class ProductIndexComponent implements OnInit {
     return direction;
   }
 
+  ajax() {
+    this.api
+      .ajax({
+        method: 'get',
+        url: '/api/product/list',
+        body: {
+          orderType: this.orderType,
+          order: this.order,
+          type: 1,
+          keyword: this.keyword,
+          appId: this.storage.get('appinfo')['id']
+        }
+      })
+      .subscribe(res => {
+        console.log(res);
+      });
+  }
+
   search(num: number) {
-    this.lineLeaveLeft = num;
+    this.lineLeaveLeft = num || this.lineLeaveLeft;
     switch (num) {
       case 2:
         this.timeDirection = this.changeDirection(this.timeDirection);
         this.saleDirection = null;
         this.priceDirection = null;
+        if (this.timeDirection === 1 || !this.timeDirection) {
+          this.orderType = 1;
+        } else {
+          this.orderType = 1;
+        }
+        this.order = 'updateTime';
+        this.ajax();
         break;
       case 4:
         this.timeDirection = null;
         this.saleDirection = this.changeDirection(this.saleDirection);
         this.priceDirection = null;
+        if (this.saleDirection === 1 || !this.saleDirection) {
+          this.orderType = 1;
+        } else {
+          this.orderType = 1;
+        }
+        this.order = 'saleCount';
+        this.ajax();
         break;
       case 6:
         this.timeDirection = null;
         this.saleDirection = null;
         this.priceDirection = this.changeDirection(this.priceDirection);
+        if (this.priceDirection === 1 || !this.priceDirection) {
+          this.orderType = 1;
+        } else {
+          this.orderType = 1;
+        }
+        this.order = 'price';
+        this.ajax();
         break;
       default:
         break;
