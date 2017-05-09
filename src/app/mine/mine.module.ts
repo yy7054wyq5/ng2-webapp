7d9100d676701dcc1cc4efac6e83b83e0a0d6836
吴阳 <yang_wu@loongjoy.com>
2017-05-09 13:46:27 +0800

    ..

--- src/app/mine/mine.module.ts
+++ src/app/mine/mine.module.ts
@@ -1,11 +1,14 @@
+import { ComponentModule } from './../component/component.module';
 import { IndexComponent } from './index/index.component';
 import { MineRoutingModule } from './mine.routing.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+
 @NgModule({
   imports: [
     CommonModule,
+    ComponentModule,
     MineRoutingModule,
   ],
   declarations: [
